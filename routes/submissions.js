23ec1b1cd7b01ad63fb0322b70947069669a95d9
Siddhant Sharma <siddhant4040s@gmail.com>
2025-10-23 22:25:45 +0530

    feat(debug):fixed issues in routes and server files

--- routes/submissions.js
+++ routes/submissions.js
@@ -1,18 +1,23 @@
 import express from 'express';
 import { Submission } from '../models/Submission.js';
 import { Router } from 'express';
+import fs from 'fs';
+import { upload } from '../middleWare/multer.js';
 const router= Router();
 
-router.post('/', async (req, res) => {
+router.post('/',upload.any(), async (req, res) => {
   try {
-    const { registrationNumber, photos } = req.body;
-    if (!registrationNumber || !photos || photos.length !== 3) {
+    console.log('Request body:', req.body);
+     console.log('Uploaded files:', req.files);
+    const { registrationNumber } = req.body;
+    const photos=req.files.map(file=>file.filename);
+     if (!registrationNumber || !photos ) {
       return res.status(400).json({ message: "Registration number and 3 photos required" });
     }
 
     const submission = new Submission({ registrationNumber, photos });
     await submission.save();
-    await fs.uploadFiles(photos, registrationNumber);
+  
     res.status(201).json({ message: "Submission successful" });
   } catch (error) {
     console.error(error);
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-require('dotenv').config();
+import dotenv from 'dotenv';
+//import multer from 'multer';
+dotenv.config();
 
 const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 import submissionsRoute from './routes/submissions.js';
@@ -22,8 +25,9 @@ app.get('/', (req, res) => {
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT ||5000, () => {
       console.log(`Server running on port ${process.env.PORT}`);
     });
   })
   .catch(err => console.error(err));
+export {app};
